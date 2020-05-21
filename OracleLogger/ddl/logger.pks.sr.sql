9e2e936ce3e22df59f5d5252c7ea5b955f7700b4
jjs <tech.wanker@gmail.com>
2020-05-21 09:08:47 -0500

    Initial commit

--- OracleLogger/ddl/logger.pks.sr.sql
+++ OracleLogger/ddl/logger.pks.sr.sql
@@ -1,3 +1,4 @@
+--%# Logger package
 set echo on
 
 --%```
@@ -12,11 +13,20 @@ create or replace PACKAGE logger AS
     G_FINER        CONSTANT PLS_INTEGER := 8 ;
     G_FINEST       CONSTANT PLS_INTEGER := 9 ;
     G_NONE         CONSTANT PLS_INTEGER := 10 ;
+
+    SEVERE       CONSTANT PLS_INTEGER := 1 ;
+    WARNING      CONSTANT PLS_INTEGER := 2 ;
+    INFO         CONSTANT PLS_INTEGER := 4 ;
+    VERBOSE      CONSTANT PLS_INTEGER := 5 ;
+    VERY_VERBOSE CONSTANT PLS_INTEGER := 6 ;
+    DEBUG        CONSTANT PLS_INTEGER := 7 ;
+    TRACE        CONSTANT PLS_INTEGER := 9 ; 
+    NONE         CONSTANT PLS_INTEGER := 10 ;
 --%```
 
 
 --%```
-    function format_time(p_timestamp in timestamp) 
+    function format_timestamp(timestamp in timestamp) 
     return varchar;
 --%```
 
@@ -33,14 +43,14 @@ create or replace PACKAGE logger AS
     procedure begin_log ( 
         logfile_name   in varchar,
         logfile_directory in varchar default 'JOB_MSG_DIR',
-        p_process_name in varchar default null,
-        p_log_set      in varchar default null,
-        p_classname    in varchar default null,
-        p_module_name  in varchar default null,
-        p_status_msg   in varchar default null,
-        p_thread_name  in varchar default null,
-        p_log_level    in pls_integer default G_INFO,
-        p_trace_level  in pls_integer default G_INFO);
+        process_name in varchar default null,
+        log_set      in varchar default null,
+        classname    in varchar default null,
+        module_name  in varchar default null,
+        status_msg   in varchar default null,
+        thread_name  in varchar default null,
+        log_level    in pls_integer default G_INFO,
+        trace_level  in pls_integer default G_INFO);
 --%```
 
 --%## Filter
@@ -51,9 +61,9 @@ create or replace PACKAGE logger AS
 --%## Log
 --%```
     procedure log (
-      p_log_msg      in   varchar,
-      p_log_level    in   pls_integer default g_info,
-      p_dump_stack   in   boolean default false
+      log_msg     in   varchar,
+      log_level    in   pls_integer default g_info,
+      dumstack   in   boolean default false
    );
 --%```
 
@@ -64,16 +74,16 @@ create or replace PACKAGE logger AS
 
 --%```
    FUNCTION begin_job ( 
-        p_process_name in varchar,
-        p_log_set      in varchar default null,
-        p_classname    in varchar default null,
-        p_module_name  in varchar default null,
-        p_status_msg   in varchar default null,
-        p_thread_name  in varchar default null,
+        process_name in varchar,
+        log_set      in varchar default null,
+        classname    in varchar default null,
+        module_name  in varchar default null,
+        status_msg   in varchar default null,
+        thread_name  in varchar default null,
         logfile_name   in varchar default null,
         logfile_directory in varchar default 'JOB_MSG_DIR',
-        p_log_level    in pls_integer default G_INFO,
-        p_trace_level  in pls_integer default G_INFO)
+        log_level    in pls_integer default G_INFO,
+        trace_level  in pls_integer default G_INFO)
         return varchar;
 --%```
 
@@ -81,10 +91,10 @@ create or replace PACKAGE logger AS
 
 --%```
    function job_step_insert (
-        p_step_name   in varchar, 
-        p_step_info   in varchar default null, 
-        p_classname   in varchar default null,     
-        p_stacktrace  in varchar default null
+        step_name   in varchar, 
+        step_info   in varchar default null, 
+        classname   in varchar default null,     
+        stacktrace  in varchar default null
    ) return number;
 --%```
 
@@ -108,19 +118,19 @@ create or replace PACKAGE logger AS
 
 --%# TODO
 --%```
-    procedure set_action (p_action in        varchar) ;
+    procedure set_action (action in        varchar) ;
 --%```
 
 --%```
     procedure set_module (
-        p_module_name in        varchar,
-        p_action_name in   varchar
+        module_name in        varchar,
+        action_name in   varchar
     );
 --%```
 
 --%# sql trace
 --%```
-    function get_tracefile(p_file_name in varchar) 
+    function get_tracefile(file_name in varchar) 
     return clob;
 --%```
 
@@ -130,7 +140,7 @@ create or replace PACKAGE logger AS
 --%```
 
 --%```
-    function set_tracefile_identifier(p_job_nbr in number) 
+    function set_tracefile_identifier(job_nbr in number) 
     return varchar;
 --%```
 
@@ -142,9 +152,9 @@ create or replace PACKAGE logger AS
 
 --%# misc
 --%```
-    function basename (p_full_path in varchar,
-                       p_suffix    in varchar default null,
-                       p_separator in char default '/') 
+    function basename (full_path in varchar,
+                       suffix    in varchar default null,
+                       separator in char default '/') 
     return varchar;
 --%```
 
@@ -153,7 +163,7 @@ create or replace PACKAGE logger AS
 --%```
 
 --%```
-    procedure trace_step(p_step_name in varchar, p_job_step_id in number);
+    procedure trace_step(step_name in varchar, job_step_id in number);
 --%```
 
 
